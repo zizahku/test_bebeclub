c137772c14bdaa05b4b5e48470f7a8a8225cd949
Shandika <Shandika@DESKTOP-NNQII6O>
2023-12-05 14:22:50 +0700

    new test commit

--- Scripts/Bebejourney/Login BebeJourney prod/Script1701760652066.groovy
+++ Scripts/Bebejourney/Login BebeJourney prod/Script1701760652066.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('') 
 
 WebUI.navigateToUrl('https://bebeclub.co.id/')
 
